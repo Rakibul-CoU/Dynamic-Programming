c8ce994767aa95ca170303ff7aa614e8a6392c2a
Md. Rakibul Islam <39064963+Rakibul-CoU@users.noreply.github.com>
2018-07-07 23:37:50 +0600

    Add files via upload

---
+++ Largest subarray sum.cpp
@@ -0,0 +1,65 @@
+/**
+    Bismillahir Rahmanir Rahim
+    Md. Rakibul Islam
+    Dept. of CSE, CoU
+    **/
+#include<bits/stdc++.h>
+#define SQR(n) (n*n)
+#define CHR getchar()
+#define NL printf("\n")
+#define PI 2.0*acos(0.0)
+#define ll              long long int
+#define ull             unsigned long long int
+#define sc              scanf
+#define gcd(a,b)        __gcd(a,b)
+#define lcm(a,b)        ((a)*(b))/gcd(a,b)
+#define pf              printf
+#define MOD             1000000007
+#define mx              123456789
+#define Max(a,b) ((a>b)?a:b)
+#define Min(a,b) ((a<b)?a:b)
+#define ff first
+#define D double
+#define sz size()
+#define ss second
+#define PB push_back
+#define MP make_pair
+#define all(a) a.begin(),a.end()
+
+#define _Max(a,b,c) Max(a,Max(b,c))
+#define _Min(a,b,c) Min(a,Min(b,c))
+
+#define BitCnt(a) __builtin_popcountll(a)
+#define MEM(a,val) memset(a,val,sizeof(a))
+using namespace std;
+int maxSubArraySum(int a[], int size)
+{
+    int max_so_far = INT_MIN, max_ending_here = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        max_ending_here = max_ending_here + a[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+}
+
+/*Driver program to test maxSubArraySum*/
+int main()
+{
+    int n, i, j, k, a[10000];
+    cin>>n;
+    for(i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    //int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+     //n = sizeof(a)/sizeof(a[0]);
+    int max_sum = maxSubArraySum(a, n);
+    cout << "Maximum contiguous sum is " << max_sum;
+    return 0;
+}
