71c649c2ae5013268df4bee5e9c64f24a01b44bd
Md. Rakibul Islam <39064963+Rakibul-CoU@users.noreply.github.com>
2018-07-08 00:37:45 +0600

    Add files via upload

---
+++ Ugly number.cpp
@@ -0,0 +1,77 @@
+/**
+    Bismillahir Rahmanir Rahim
+    Md. Rakibul Islam
+    Dept. of CSE, CoU
+    **/
+#include<bits/stdc++.h>
+#define SQR(n) (n*n)
+#define CHR getchar()
+#define NL printf("\n")
+#define PI 2.0*acos(0.0)
+#define ll              long long int
+#define ull             unsigned long long int
+#define sc              scanf
+#define gcd(a,b)        __gcd(a,b)
+#define lcm(a,b)        ((a)*(b))/gcd(a,b)
+#define pf              printf
+#define MOD             1000000007
+#define mx              123456789
+#define Max(a,b) ((a>b)?a:b)
+#define Min(a,b) ((a<b)?a:b)
+#define ff first
+#define D double
+#define sz size()
+#define ss second
+#define PB push_back
+#define MP make_pair
+#define all(a) a.begin(),a.end()
+
+#define _Max(a,b,c) Max(a,Max(b,c))
+#define _Min(a,b,c) Min(a,Min(b,c))
+
+#define BitCnt(a) __builtin_popcountll(a)
+#define MEM(a,val) memset(a,val,sizeof(a))
+using namespace std;
+/* Function to get the nth ugly number*/
+unsigned getNthUglyNo(unsigned n)
+{
+    unsigned ugly[n]; // To store ugly numbers
+    unsigned i2 = 0, i3 = 0, i5 = 0;
+    unsigned next_multiple_of_2 = 2;
+    unsigned next_multiple_of_3 = 3;
+    unsigned next_multiple_of_5 = 5;
+    unsigned next_ugly_no = 1;
+
+    ugly[0] = 1;
+    for (int i=1; i<n; i++)
+    {
+       next_ugly_no = min(next_multiple_of_2,
+                           min(next_multiple_of_3,
+                               next_multiple_of_5));
+       ugly[i] = next_ugly_no;
+       if (next_ugly_no == next_multiple_of_2)
+       {
+           i2 = i2+1;
+           next_multiple_of_2 = ugly[i2]*2;
+       }
+       if (next_ugly_no == next_multiple_of_3)
+       {
+           i3 = i3+1;
+           next_multiple_of_3 = ugly[i3]*3;
+       }
+       if (next_ugly_no == next_multiple_of_5)
+       {
+           i5 = i5+1;
+           next_multiple_of_5 = ugly[i5]*5;
+       }
+    } /*End of for loop (i=1; i<n; i++) */
+    return next_ugly_no;
+}
+
+/* Driver program to test above functions */
+int main()
+{
+    int n = 150;
+    cout << getNthUglyNo(n);
+    return 0;
+}
